f9a5a0b17e24242450c11f55ec1c7e540939bb27
EvanieWares <evaniewares@gmail.com>
2024-09-16 14:21:48 +0000

    Add full budget overview

--- back-end/app/models/budget.py
+++ back-end/app/models/budget.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 class Budget(BaseModel):
     limit_amount: float
@@ -8,6 +8,26 @@ class Budget(BaseModel):
     start_date: datetime
     end_date: datetime
 
+    @field_validator("limit_amount", check_fields=False)
+    def round_money(cls, value):
+        return round(value, 2)
+
 
 class BudgetInDB(Budget):
     id: str
+
+
+class BudgetBreakdown(BaseModel):
+    limit_amount: float
+    used_amount: float
+    balance: float
+    percentage: float
+
+    @field_validator("limit_amount", "used_amount", "balance", "percentage")
+    def round_values(cls, value):
+        return round(value, 2)
+
+
+class FullBudget(BaseModel):
+    budget: BudgetInDB
+    breakdown: BudgetBreakdown
