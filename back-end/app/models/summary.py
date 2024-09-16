8e1064c0e6b8b163bbf15668829ce4f21a776567
EvanieWares <evaniewares@gmail.com>
2024-09-16 14:22:56 +0000

    Add summary model validator for money

--- back-end/app/models/summary.py
+++ back-end/app/models/summary.py
@@ -1,7 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class Summary(BaseModel):
     total_income: float
     total_expenses: float
     net_savings: float
+
+    @field_validator("total_income", "total_expenses", "net_saving", check_fields=False)
+    def round_money(cls, value):
+        return round(value, 2)
