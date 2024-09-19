8e9cef3cf441ad1802d3c1d02c8a0a64b06c6b30
EvanieWares <evaniewares@gmail.com>
2024-09-19 03:26:52 +0000

    Return token when registered a new user

--- back-end/app/routes/auth.py
+++ back-end/app/routes/auth.py
@@ -1,14 +1,11 @@
-from datetime import timedelta
-from typing import Any, Annotated
+from typing import Annotated
 
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
 from ..models.token import Token
-from ..models.user import User, UserInDB
-from ..crud.user import create_user, authenticate_user
-from ..core.security import create_access_token
-from ..core.config import settings
+from ..models.user import UserInDB
+from ..crud.user import create_user, authenticate_user, get_access_token
 
 router = APIRouter(
     prefix="/auth",
@@ -26,14 +23,11 @@ async def login(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
 ) -> Token:
     user = authenticate_user(form_data.username, form_data.password)
-    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return Token(access_token=access_token, token_type="bearer")
+    return get_access_token(user.username)
 
 
-@router.post("/register", response_model=User, status_code=201)
-async def register(user: UserInDB) -> Any:
+@router.post("/register", status_code=201)
+async def register(user: UserInDB) -> Token:
     """Register a new user."""
-    return create_user(user)
+    new_user = create_user(user)
+    return get_access_token(new_user.username)
