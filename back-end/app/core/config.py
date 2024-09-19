b9a009275e039653e135e2a2cb91a63b631840ac
EvanieWares <evaniewares@gmail.com>
2024-09-19 12:09:17 +0000

    Cleaned up some unused code

--- back-end/app/core/config.py
+++ back-end/app/core/config.py
@@ -1,14 +1,8 @@
-import os
 import secrets
-
-from dotenv import load_dotenv
-
-load_dotenv("app/.env")
 
 
 class Settings():
     def __init__(self) -> None:
-        self.MONGO_REMOTE_URI = os.getenv("MONGO_REMOTE_URI")
         self.SECRET_KEY = secrets.token_urlsafe(32)
         self.ALGORITHM = "HS256"
         self.ACCESS_TOKEN_EXPIRE_MINUTES = 30
